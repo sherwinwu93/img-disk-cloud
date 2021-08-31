57b365f7128a0f1c2614e04b9f283019453c24a0
wusd <wsd5656126@foxmail.com>
2021-08-31 20:02:22 +0800

    --

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,5 +1,37 @@
 <template>
   <div>
+  <!--顶部信息栏:工作表sheet-->
+    <v-sheet
+        class="bar d-flex justify-end align-center"
+        height="45"
+        color="#0808084d"
+    >
+      <div class="pt-2">
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <a v-bind='attrs' v-on='on'  href="https://gitee.com/onlypluto/image-cloud-disk"
+            ><img src="@/assets/img/github.png"
+            /></a>
+          </template>
+          <span>代码仓库</span>
+        </v-tooltip>
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <a v-bind='attrs' v-on='on'><img @click="contackMe = true" src="@/assets/img/weixin.png"/></a>
+          </template>
+          <span>联系维护者</span>
+        </v-tooltip>
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <a v-bind='attrs' v-on='on' href="https://www.alonepluto.info/free"
+            ><img src="@/assets/img/find.png"
+            /></a>
+          </template>
+          <span>发现更多</span>
+        </v-tooltip>
+
+      </div>
+    </v-sheet>
   </div>
 </template>
 
@@ -10,18 +42,8 @@ export default {
     return {
       username: "",
       password: "",
-      // 显示登陆还是注册
+      // 控制显示
       type: "login",
-      set_username: "",
-      set_password: "",
-      set_password_again: "",
-      // 密保问题
-      items:[],
-      question: "",
-      answer: "",
-      resetpassword_answer: "",
-      reset_type: 1,
-      contackMe: false,
     }
   }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,16 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    snackbar: false,
+    snackbar_timeout: 3000,
+    snackbar_color: "",
+    snackbar_text: "",
   },
   mutations: {
+    message(state, payload) {
+      state.snackbar = true;
+      state.snackbar_color = payload.color;
+      state.snackbar_text = payload.text;
+    }
   }
 })
